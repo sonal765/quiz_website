de01826eecf67d8875c706ac4689cad3c82b8949
sonal765 <sonalj7657@gmail.com>
2023-04-17 19:05:36 +0530

    Online database connection

--- backend/src/db/conn.js
+++ backend/src/db/conn.js
@@ -1,7 +1,16 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/quizRegistration").then(()=>{
+//mongo ATLAS used
+const DB = 'mongodb+srv://sonalj7657:mongoquiz@cluster0.amspoa3.mongodb.net/Quiz?retryWrites=true&w=majority';
+mongoose.connect(DB).then(()=>{
     console.log(`Connection successful`);
-}).catch((e)=>{
-    console.log(e);
+    }).catch((e)=>{
+        console.log(e);
 })
+
+//local uri used(MONGODB COMPASS)
+// mongoose.connect("mongodb://127.0.0.1:27017/quizRegistration").then(()=>{
+//     console.log(`Connection successful`);
+// }).catch((e)=>{
+//     console.log(e);
+// })
